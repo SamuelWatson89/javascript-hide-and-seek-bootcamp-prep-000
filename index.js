a2cd8b3f1a787222593ecfd7735998964606a510
Samuel Watson <otacon100@hotmail.com>
2018-07-14 18:37:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(div) {
 }
 
 function nestedTarget(div) {
-  return document.querySelector('div.nested div div div#target')
+  return document.querySelector('div#nested div div div.target')
 }
